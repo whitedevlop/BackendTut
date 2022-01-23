a11bc16280b2fc9a47644701f04e3890c431ae78
whitedev <sanamrai1008@gmail.com>
2022-01-23 21:25:24 +0545

    Aggregate Calculate average rating

---
+++ routes/reviews.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const { getReviews, getReview, addReview } = require("../controllers/reviews");
+
+const Review = require("../models/Review");
+
+const router = express.Router({ mergeParams: true });
+
+const advancedResults = require("../middleware/advancedResults");
+const { protect, authorize } = require("../middleware/auth");
+
+router
+  .route("/")
+  .get(
+    advancedResults(Review, {
+      path: "bootcamp",
+      select: "name description",
+    }),
+    getReviews
+  )
+  .post(protect, authorize("user", "admin"), addReview);
+
+router.route("/:id").get(getReview);
+
+module.exports = router;
--- seeder.js
+++ seeder.js
@@ -10,6 +10,7 @@ dotenv.config({ path: "./config/config.env" });
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
 const User = require("./models/User");
+const Review = require("./models/Review");
 
 // Connect to Database
 mongoose.connect(process.env.MONGO_URI, {
@@ -30,12 +31,17 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
 
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, "utf-8")
+);
+
 // Import in to Db
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
     console.log("Data imported...".green.inverse);
     process.exit();
   } catch (err) {
@@ -49,6 +55,7 @@ const deleteData = async () => {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
     await User.deleteMany();
+    await Review.deleteMany();
     console.log("Data Destroyed.".red.inverse);
     process.exit();
   } catch (err) {
